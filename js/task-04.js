4d48ffa1907bfc6f031d04f66113e68c91814d92
bernatskak <106559001+bernatskak@users.noreply.github.com>
2022-11-03 23:29:37 +0200

    refs

--- js/task-04.js
+++ js/task-04.js
@@ -1,24 +1,19 @@
+const refs = {
+  value: document.querySelector(".value"),
+  increment: document.querySelector(".increment"),
+  decrement: document.querySelector(".decrement"),
+};
+let counterValue = 0;
 
-   const refs = {
-    value: document.querySelector('.value'),
-    increment: document.querySelector('.increment'),
-    decrement: document.querySelector('.decrement'),
+const decrementValue = () => {
+  counterValue -= 1;
+  refs.value.textContent = counterValue;
+};
 
-  };
-  let counterValue = 0;
+const incrementValue = () => {
+  counterValue += 1;
+  refs.value.textContent = counterValue;
+};
 
-  const render = () => {
-    refs.value.textContent = counterValue;
-  };
-  const decrementValue = () => {
-    counterValue -= 1;
-    render();
-  };
-
-  const incrementValue = () => {
-    counterValue += 1;
-    render();
-  };
-
-  refs.decrement.addEventListener('click', decrementValue());
-  refs.increment.addEventListener('click', incrementValue());
+refs.decrement.addEventListener("click", decrementValue());
+refs.increment.addEventListener("click", incrementValue());
