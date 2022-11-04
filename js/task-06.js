5003ebdd0eb0d660498e52641841d368e5d6f2d6
bernatskak <106559001+bernatskak@users.noreply.github.com>
2022-11-05 00:06:20 +0200

    finish task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,14 @@
 const refs = {
-  inputEl: document.querySelector("#validator-input"),
+  styleEl: document.querySelector("style"),
+  inputEl: document.querySelector("#validation-input"),
 };
-const symbols = Number(refs.inputEl.data - length);
-console.log(symbols);
+const symbols = Number(refs.inputEl.dataset.length);
+
+refs.inputEl.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
   console.log(event.currentTarget.value.length);
   event.currentTarget.value.length === symbols
     ? refs.inputEl.classList.add("valid")
-    : refs.inputEl.classList.add("invalid");
+    : refs.inputEl.classList.remove("invalid");
 }
-refs.inputEl.addEventListener("blur", onInputBlur);
