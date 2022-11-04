10c944a9d5d52548ff100f5f1bafbd6f84282b99
bernatskak <106559001+bernatskak@users.noreply.github.com>
2022-11-04 22:32:49 +0200

    task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,16 +1,6 @@
-// const refs = {
-//   textInput: document.querySelector(".text-input"),
-//   output: document.querySelector(".output"),
-// };
-// function onTextInput(event) {
-//   console.log(event.currentTarget.value);
-//   refs.textInput.textContent = event.currentTarget.value;
-// }
-// refs.textInput.addEventListener("input", onTextInput);
-const refs = (event) => {
-  refs.textInput.value.length === "0"
-    ? (refs.output.textContent = "Anpnimus")
-    : (refs.output.textContent = event.currentTarget.value);
-};
-console.log(refs.output);
-refs.textInput.addEventListener("input", refs);
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+textInput.addEventListener("input", (event) => {
+  output.textContent = event.currentTarget.value;
+});
