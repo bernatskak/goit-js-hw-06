63a9d4646f48e54bc2d9e85de6271aa60e65f289
bernatskak <106559001+bernatskak@users.noreply.github.com>
2022-11-05 01:22:10 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector("body"),
+  span: document.querySelector(".color"),
+  button: document.querySelector(".change-color"),
+};
+const SetBg = (event) => {
+  const randomColor = getRandomHexColor();
+  refs.body.style.backgroundColor = randomColor;
+  refs.span.innerHTML = randomColor;
+};
+
+refs.button.addEventListener("click", setBg);
