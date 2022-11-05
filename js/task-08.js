2a1f0b8fdff3fb4a400ae0f9a3711c320f8461e7
bernatskak <106559001+bernatskak@users.noreply.github.com>
2022-11-05 22:23:38 +0200

    1

--- js/task-08.js
+++ js/task-08.js
@@ -1,21 +1,3 @@
-// const form = document.querySelector(".login-form");
-
-// form.addEventListener("submit", handleSubmit);
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-
-//   const {
-//     elements: { login, password },
-//   } = event.currentTarget;
-
-//   if (login.value === "" || password.value === "") {
-//     return alert(`Please fill in all the fields!`);
-//   }
-//   const userDetails = { login: email.value, Password: password.value };
-//   console.log(userDetails);
-//   event.currentTarget.reset();
-// }
 const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", handleSubmit);
