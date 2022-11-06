89187bab43f1f242d299b66cdbc0b8eeb8288952
bernatskak <106559001+bernatskak@users.noreply.github.com>
2022-11-06 23:59:04 +0200

    +

--- js/task-05.js
+++ js/task-05.js
@@ -3,7 +3,7 @@ const output = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
   output.textContent = event.currentTarget.value;
-  if (event.currentTarget.value === "") {
-    output.textContent = "Anonymous";
+  if (event.currentTarget.value === "0") {
+    output.textContent.trim() = "Anonymous";
   }
 });
