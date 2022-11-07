38e4741e38fc94ecff0bc50c9cc7c11d193116a2
bernatskak <106559001+bernatskak@users.noreply.github.com>
2022-11-08 00:14:25 +0200

    task6

--- js/task-06.js
+++ js/task-06.js
@@ -1,32 +1,16 @@
-// const textInput = document.querySelector("#validation-input");
-// console.log(textInput.getAttribute("data-length"));
+const inputEl = document.querySelector("#validation-input");
 
-// textInput.addEventListener("blur", (event) => {
-//   if (
-//     event.currentTarget.value.length === textInput.getAttribute("data-length")
-//   ) {
-//     textInput.classList.add("valid");
-//     if (textInput.classList.contains("invalid")) {
-//       textInput.classList.remove("invalid");
-//     }
-//   } else {
-//     if (textInput.classList.contains("valid")) {
-//       textInput.classList.remove("valid");
-//     }
-//     textInput.classList.add("invalid");
-//   }
-// });
-const refs = {
-  styleEl: document.querySelector("style"),
-  inputEl: document.querySelector("#validation-input"),
-};
-const symbols = Number(refs.inputEl.dataset.length);
+const symbols = Number(inputEl.dataset.length);
 
-refs.inputEl.addEventListener("blur", onInputBlur);
+inputEl.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
-  console.log(event.currentTarget.value.length);
-  event.currentTarget.value.length === symbols
-    ? refs.inputEl.classList.add("valid")
-    : refs.inputEl.classList.add("invalid");
+  // console.log(event.currentTarget.value.length);
+  if (event.currentTarget.value.length === symbols) {
+    inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
+  } else {
+    inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
+  }
 }
