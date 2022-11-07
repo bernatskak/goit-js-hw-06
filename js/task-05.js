df3f0374339f0c406eb51332e261b9408b71c524
bernatskak <106559001+bernatskak@users.noreply.github.com>
2022-11-07 23:30:17 +0200

    task 5 add trim

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,8 @@ const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value;
-  if (event.currentTarget.value === "0") {
-    output.textContent.trim() = "Anonymous";
+  output.textContent = event.currentTarget.value.trim();
+  if (event.currentTarget.value.trim() === "") {
+    output.textContent = "Anonymous";
   }
 });
--- js/task-06.js
+++ js/task-06.js
@@ -1,18 +1,32 @@
-const textInput = document.querySelector("#validation-input");
-console.log(textInput.getAttribute("data-length"));
+// const textInput = document.querySelector("#validation-input");
+// console.log(textInput.getAttribute("data-length"));
 
-textInput.addEventListener("blur", (event) => {
-  if (
-    event.currentTarget.value.length == textInput.getAttribute("data-length")
-  ) {
-    textInput.classList.add("valid");
-    if (textInput.classList.contains("invalid")) {
-      textInput.classList.remove("invalid");
-    }
-  } else {
-    if (textInput.classList.contains("valid")) {
-      textInput.classList.remove("valid");
-    }
-    textInput.classList.add("invalid");
-  }
-});
+// textInput.addEventListener("blur", (event) => {
+//   if (
+//     event.currentTarget.value.length === textInput.getAttribute("data-length")
+//   ) {
+//     textInput.classList.add("valid");
+//     if (textInput.classList.contains("invalid")) {
+//       textInput.classList.remove("invalid");
+//     }
+//   } else {
+//     if (textInput.classList.contains("valid")) {
+//       textInput.classList.remove("valid");
+//     }
+//     textInput.classList.add("invalid");
+//   }
+// });
+const refs = {
+  styleEl: document.querySelector("style"),
+  inputEl: document.querySelector("#validation-input"),
+};
+const symbols = Number(refs.inputEl.dataset.length);
+
+refs.inputEl.addEventListener("blur", onInputBlur);
+
+function onInputBlur(event) {
+  console.log(event.currentTarget.value.length);
+  event.currentTarget.value.length === symbols
+    ? refs.inputEl.classList.add("valid")
+    : refs.inputEl.classList.add("invalid");
+}
